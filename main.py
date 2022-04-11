805e4965996e05c3cbc2f00b18cfb815edf45488
heigon77 <h217638@dac.unicamp.br>
2022-04-11 13:41:47 -0300

    Playing chess

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from Game.ChessMain import Game
+import chess as ch
 
-Game.play()
+game = Game()
+game.play()
+
+# board = ch.Board()
+# piece = board.piece_at(63)
+# if piece is not None:
+#     if piec:
+#         print("Boa")
+#     print(piece)
+#     print(piece.piece_type)
+#     print(piece.color)
+# else:
+#     print(piece)
