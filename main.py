bd9c524c38235212c5848e815feda060900bbe32
heigon77 <h217638@dac.unicamp.br>
2022-04-13 17:27:26 -0300

    Adding EngineVsEngine.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from Game.ChessMain import Game
 from Game.DumbChessEngine import DumbChessEngine
 from Game.DailyPuzzle import DailyPuzzle
+from Game.EngineVsEngine import EngineVsEngine
+
 import chess as ch
 
 if __name__ == '__main__':
 
-    mode = input("Type 'n' for new game or 'p' for a random chess.com daily puzzle \n")
+    mode = input("Type 'n' for new game, 'p' for a random chess.com daily puzzle or 'e' for Engine vs Engine \n")
 
     if mode == 'p':
         Game(DumbChessEngine(), DailyPuzzle()).play()
+    if mode == 'e':
+        EngineVsEngine(DumbChessEngine(), DumbChessEngine()).play()
     else:
         color = input("Type 'b' for black or 'w' for white\n")
         if color == 'b':
